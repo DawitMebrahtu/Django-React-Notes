4c8f07f23fa2ccf0fe33e3fc951a7018e9d42bca
DawitMebrahtu <dawitmebrahtu93@gmail.com>
2023-07-18 08:43:43 +0300

    Set up serializers

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import getRoutes 
+from .views import getRoutes, getNotes, getNote
 
 urlpatterns = [
-		path('', views.getRoutes, name ='routes')
+	path('', getRoutes, name ='routes'),
+	path('notes/', getNotes, name='notes'),
+	path('notes/<int:note_id>', getNote, name='note'),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,54 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Notes
+from .serializers import NoteSerializer
 # Create your views here.
 
-
+@api_view(['GET'])
 def getRoutes(request):
-	routes = []
-	return JsonResponse(routes, safe=True)
+	routes = [
+		{
+			'Endpoint': '/notes/',
+			'method': 'GET',
+			'body': None,
+			'description': 'Returns an array of notes'
+		},
+		{
+			'Endpoint': '/notes/id',
+			'method': 'GET',
+			'body': None,
+			'description': 'Returns a single note object'
+		},
+		{
+			'Endpoint': '/notes/create/',
+			'method': 'POST',
+			'body': {'body': ""},
+			'description': 'Creates new note with data sent in post request'
+		},
+		{
+			'Endpoint': '/notes/id/update/',
+			'method': 'PUT',
+			'body': {'body': ""},
+			'description': 'Creates an existing note with data sent in post request'
+		},
+		{
+			'Endpoint': '/notes/id/delete/',
+			'method': 'DELETE',
+			'body': None,
+			'description': 'Deletes and exiting note'
+		},
+	]
+	return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+	notes = Notes.objects.all()
+	serializer = NoteSerializer(notes, many=True)
+	return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, note_id):
+	note = Notes.objects.get(id=note_id)
+	serializer = NoteSerializer(note, many=False)
+	return Response(serializer.data)
